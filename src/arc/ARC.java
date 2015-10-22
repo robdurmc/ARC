ba0cb7b7725ae90e687fdbb96a8c6b1dc509ef28
Samuel-MBP <Samuel@192.168.1.16>
2015-10-22 12:37:16 +0200

    Añadido por que te rallatis en comentario

---
+++ src/arc/ARC.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arc;
+
+/**
+ *
+ * @author Samuel
+ */
+public class ARC {
+
+    /**
+     * @param args the command line arguments
+     * por que te rallatis
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
